7b62db0d62080be59fdcd8aaad070f0280887979
YvesCousteau <hugohenrotte@hotmail.com>
2023-01-23 15:13:32 +0100

    tailwind

---
+++ software/test_tailwind/my-app/src/pages/Home.js
@@ -0,0 +1,14 @@
+function Home() {
+    return (
+        <div className="px-8 pt-4">
+            <p className="text-3xl text-gray-7àà font-bold mb-5">
+                Welcome !
+            </p>
+            <p className="text-gray-500 text-lg">
+                React and Tailwind CSS in action
+            </p>
+        </div>
+    );
+}
+
+export default Home;
