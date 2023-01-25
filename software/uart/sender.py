1483764e6f64b902961d8adae3d096d2f0c197b9
YvesCousteau <hugohenrotte@hotmail.com>
2023-01-25 12:02:26 +0100

    sheeeeeeeeeesh

--- software/uart/sender.py
+++ software/uart/sender.py
@@ -1,19 +1,19 @@
 # sender.py
 import time
 import serial
+import sys
 ser = serial.Serial(
   port='/dev/ttyS0', # Change this according to connection methods, e.g. /dev/ttyUSB0
-  baudrate = 9600,
+  baudrate = 115200,
   parity=serial.PARITY_NONE,
   stopbits=serial.STOPBITS_ONE,
   bytesize=serial.EIGHTBITS,
+  timeout=1
 )
 msg = ""
 i = 0
 while True:
     i+=1
-    print(i)
-    # 1=>g 3=>f 5=>e 7=>d 9=>c
-    #ser.write(('6').encode("ASCII"))
-    ser.write(("1").encode())
+    print("Counter {} - from Raspberry Pi - ".format(i) + sys.argv[1])
+    ser.write(sys.argv[1].encode())
     time.sleep(2)
