fe9df50d0921e93d164f60d802dc69d1c73d24ad
YvesCousteau <hugohenrotte@hotmail.com>
2023-01-26 10:25:36 +0100

    zad

--- software/ihm/server/index.js
+++ software/ihm/server/index.js
@@ -1,27 +1,24 @@
 const express = require("express")
 const path = require('path');
 const bodyParser = require("body-parser");
-const { sensitiveHeaders } = require("http2");
 const { exec } = require('child_process')
-
-const PORT = process.env.PORT || 3001
 const app = express()
 
+const PORT = process.env.PORT || 3001
 
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-
+// The body-parser middleware
+// to parse form data
+app.use(bodyParser.urlencoded({extended: true}));
 
 /* Run Build */
 // Have Node serve the files for our built React app
 //app.use(express.static(path.resolve(__dirname, './../com_manager/build')));
-
-// Handle GET requests to /api route
+/* --------------------------------------------- */
 app.get("/api", (req, res) => {
     res.json({ message: "Server is UP !" });
 });
 
+/* Need opti */
 app.post("/close_uart", (req, res) => {
     res.json({ message: "closing ..." });
     exec('kill -9 `ps aux | grep ../../uart/sender.py | awk \'{print $2}\' | head -n 1`');
@@ -29,34 +26,30 @@ app.post("/close_uart", (req, res) => {
     exec('kill -9 `ps aux | grep ../../uart/sender.py | awk \'{print $2}\' | head -n 1`');
 });
 
-// Handle GET requests to /test route
 app.post("/max7219_scrolling", (req, res) => {
     res.json({ message: "Max7219 scrolling is running ..." });
-    exec('python3 ../../uart/sender.py A', (err, output) => {
-        if (err) {
-            console.error("could not execute command: ", err)
-            return
-        }
-        console.log("Output: \n", output)
-    })
+    exec('python3 ../../uart/sender.py A')
 });
 
 app.post("/led_blinking", (req, res) => {
     res.json({ message: "LED blancking is running ..." });
-    exec('python3 ../../uart/sender.py C', (err, output) => {
-        if (err) {
-            console.error("could not execute command: ", err)
-            return
-        }
-        console.log("Output: \n", output)
-    })
+    exec('python3 ../../uart/sender.py C')
 });
 
-// All other GET requests not handled before will return our React app
+app.post('/signup', async (req, res) => {
+    const { email, password } = req.body
+
+    const addedRecord = await repo.createNewRecord({ email, password })
+
+    console.log(`Added Record : ${JSON.stringify(addedRecord, null, 4)}`)
+
+    res.send("Information added to the database successfully.")
+});
+
 app.get('*', (req, res) => {
     res.json({ message: "Error" });
 });
-
+/* --------------------------------------------- */
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
