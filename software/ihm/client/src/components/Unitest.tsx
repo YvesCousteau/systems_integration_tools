6aedc62003658e7e9fc2f894f0427b2aa8e442d4
YvesCousteau <hugohenrotte@hotmail.com>
2023-01-27 11:13:59 +0100

    Add delete function

--- software/ihm/client/src/components/Unitest.tsx
+++ software/ihm/client/src/components/Unitest.tsx
@@ -55,39 +55,47 @@ export default function Unitest(props) {
     
 
     return(
-        <div className="rounded-[14px] shadow-md px-4 py-4 bg-gray-300 mx-auto w-60">
-            <p className="font-semibold text-gray-800 pb-1">
-                Fct : {data.function}
-            </p>
-            <p className="font-semibold text-gray-800 pb-1">
-                Device : {!data.device ? "Loading .." : data.device}
-            </p>
-            <p className="flex font-semibold text-gray-800 pb-1">
-                State :&nbsp;{data.state?(<div className='text-green-700'>Valid</div>):(<div className='text-red-700'>Error</div>)}
-            </p>
-            <button className="btn btn-classic w-full" onClick={() => setModal(true)}>
-                Run
-            </button>
-            <Modal 
-            open={modal} 
-            setOpen={setModal}
-            title={data.function}
-            subtitle="La j ai r">
-                <p className="flex justify-center text-xl font-bold color-classic">
-                    State run :&nbsp;{data.state ? (<div className='text-green-700'>Actif</div>) : (<div className='text-red-700'>Inactif</div>)}
-                </p>
-                <div className='flex px-8 pt-4'>
-                    <div className='bg-gray-300 py-4 rounded-[12px] grid grid-cols-3 w-full '>
-                        <div className='mx-auto text-lg font-semibold'>Result :</div>
-                        <button className='w-24 btn btn-open' onClick={() => setUpdate({ activity: true, value: true })}>
-                            Valid
-                        </button>
-                        <button className='w-24 btn btn-close' onClick={() => setUpdate({ activity: true, value: false })}>
-                            Unvalid
-                        </button>
+        <div>
+            {data !== null && (
+                <div className="rounded-[14px] shadow-md px-3 py-3 bg-gray-300 mx-auto w-80">
+                    <div className='flex justify-between w-full'>
+                        <p className="font-semibold text-gray-800 pb-1">
+                            Fct : {data.function}
+                        </p>
+                        <button className='btn btn-close' onClick={() => setDel(true)}>X</button>
                     </div>
+
+                    <p className="font-semibold text-gray-800 pb-1">
+                        Device : {!data.device ? "Loading .." : data.device}
+                    </p>
+                    <p className="flex font-semibold text-gray-800 pb-1">
+                        State :&nbsp;{data.state ? (<div className='text-green-700'>Success</div>) : (<div className='text-red-700'>Fail</div>)}
+                    </p>
+                    <button className="btn btn-classic w-full" onClick={() => setModal(true)}>
+                        Run
+                    </button>
+                    <Modal
+                        open={modal}
+                        setOpen={setModal}
+                        title={data.function}
+                        subtitle="La j ai r">
+                        <p className="flex justify-center text-xl font-bold color-classic">
+                            State :&nbsp;{data.state ? (<div className='text-green-700'>Success</div>) : (<div className='text-red-700'>Fail</div>)}
+                        </p>
+                        <div className='bg-gray-300 py-4 my-2 rounded-[12px] mx-4'>
+                            <div className='grid grid-cols-3 w-full'>
+                                <div className='mx-auto text-lg font-semibold'>Result :</div>
+                                <button className='w-24 btn btn-open' onClick={() => setUpdate({ activity: true, value: true })}>
+                                    Success
+                                </button>
+                                <button className='w-24 btn btn-close' onClick={() => setUpdate({ activity: true, value: false })}>
+                                    Fail
+                                </button>
+                            </div>
+                        </div>
+                    </Modal>
                 </div>
-            </Modal>
+            )}
         </div>
     );
 }
