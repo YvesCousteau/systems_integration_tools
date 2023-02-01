cb7f22e13dd5efb7715a652207ad163504a100b1
YvesCousteau <hugohenrotte@hotmail.com>
2023-02-02 00:09:23 +0100

    multi thread and share var

--- software/ihm/server/exec/server.py
+++ software/ihm/server/exec/server.py
@@ -2,25 +2,38 @@ import socket
 import sys
 sys.path.insert(1, '../functions/')
 import uart
+import json
+import threading
+import queue
 
-port = 20001
-bufferSize = 1024
-ip = sys.argv[1]
+def server(queue):
+    port = 20001
+    bufferSize = 1024
+    ip = sys.argv[1]
 
-# Create a datagram socket
-sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-print("Socket created ...")
-# Bind to address and ip
-sock.bind((ip, port))
-print("Server up and listening on : "+ip+" / "+str(port))
-# Listen for incoming datagrams
-while(True):
-    bytesAddressPair = sock.recvfrom(bufferSize)
-    message = bytesAddressPair[0].decode()
-    address = bytesAddressPair[1]
-    clientMsg = "Message from Client >> {}".format(message)
-    clientIP  = "Client IP Address:{}".format(address)
-    print(clientMsg)
-    print(clientIP)
-    
-    # uart.sender(clientMsg.value,clientMsg.stop)
+    # Create a datagram socket
+    sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    print("Socket created ...")
+    # Bind to address and ip
+    sock.bind((ip, port))
+    print("Server up and listening on : "+ip+" / "+str(port))
+    # Listen for incoming datagrams
+    while(True):
+        bytesAddressPair = sock.recvfrom(bufferSize)
+        message = json.loads(bytesAddressPair[0].decode())
+        address = bytesAddressPair[1]
+        clientMsg = "Message from Client >> {}".format(message)
+        clientIP  = "Client IP Address:{}".format(address)
+        print(clientMsg)
+        print(clientIP)
+        queue.put(message)
+
+clientMsg = ""
+
+queue = queue.Queue()
+serv = threading.Thread(target=server, args=(queue,))
+function = threading.Thread(target=uart.sender, args=(queue,))
+
+serv.start()
+function.start()
+
--- software/ihm/server/functions/uart.py
+++ software/ihm/server/functions/uart.py
@@ -2,8 +2,9 @@
 import time
 import serial
 import sys
+import queue
 
-def sender(msg,stop):
+def sender(queue):
   ser = serial.Serial(
   port='/dev/ttyS0', # Change this according to connection methods, e.g. /dev/ttyUSB0
   baudrate = 115200,
@@ -13,10 +14,8 @@ def sender(msg,stop):
   timeout=1
   )
   while True:
-    print(stop)
-    if stop:
-      return
-    else:
-      ser.write(msg.encode())
-      time.sleep(2)
+    data = queue.get()
+    print("On est laaaa " + data["value"])
+    #ser.write(msg.encode())
+    time.sleep(1)
 
