53c925fd0070bf8d638e8cf6759a468d802db090
YvesCousteau <hugohenrotte@hotmail.com>
2023-03-03 12:36:11 +0100

    ter

--- software/cluster_can/client/src/App.tsx
+++ software/cluster_can/client/src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import * as io from "socket.io-client";
 import logo  from "./Cluster_Basic.png";
 
-const socket = io.connect('http://192.168.5.40:3001');
+const socket = io.connect('http://192.168.5.40:6001');
 
 function App() {
   const [messages, setMessages] = useState(0)
