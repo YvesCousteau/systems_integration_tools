c0a98ab52953fd3e673dd9e3f77c8ea3ef10266d
YvesCousteau <hugohenrotte@hotmail.com>
2023-03-08 08:07:34 +0000

    clinique

---
+++ software/brightness/brightness.sh
@@ -0,0 +1,17 @@
+#! /bin/Bash
+# Brightness Change for Linux with xrandr (X11)
+if [[ $1 -gt 1.0 ]] || [[$1 -lt 0.1 ]]
+then
+    echo "Brightness must be between 0.1 - 1.0"
+    exit 1
+fi
+
+SCREEN = xrandr | grep " connected" | cut -f1 -d "" | tail -1
+if [[ -z "$SCREEN" ]]
+then
+    echo "No SCREEN Found"
+    exit 1
+fi
+echo "An expected brightness change of `expr $1 / 100`% for the $SCREEN"
+
+xrandr --output $SCREEN --brightness $1
