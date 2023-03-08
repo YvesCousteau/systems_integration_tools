471b8f3f7c7fef6403ea64fa2106206c0e51f803
YvesCousteau <hugohenrotte@hotmail.com>
2023-03-08 21:23:41 +0100

    zszszs

--- software/brightness/uart.py
+++ software/brightness/uart.py
@@ -1,21 +1,23 @@
 import serial
 import subprocess
+import time
 
 def uart():
     ser = serial.Serial(
         port='/dev/ttyUSB0',
-        baudrate = 115200,
+        baudrate = 9600,
         parity=serial.PARITY_NONE,
         stopbits=serial.STOPBITS_ONE,
         bytesize=serial.EIGHTBITS,
         timeout=0
     )
-    lux = 50
    
     while True:
-        lux = ser.readline()
-        subprocess.run(["brightness", "-la"])
+        lux = ser.readline().decode().rstrip()
+        print(lux)
+        subprocess.run(["brightness.sh", lux])
         ser.flush()
+        time.sleep(10)
 
 if __name__ == '__main__':
     uart()
