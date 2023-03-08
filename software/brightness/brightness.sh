4e76a9c1d299c268ca722f5c0d92fea4ad9d597b
YvesCousteau <hugohenrotte@hotmail.com>
2023-03-08 09:38:33 +0100

    clinique

--- software/brightness/brightness.sh
+++ software/brightness/brightness.sh
@@ -1,19 +1,26 @@
-#! /bin/bash
+#! /usr/bin/env bash
 # Brightness Change for Linux with xrandr (X11)
-if [[ $1 -gt 100 ]] || [[$1 -lt 1 ]]
-then
-    echo "Brightness must be between 1 - 100"
+
+if [[ $# -eq 0 ]] ; then
+    echo 'No brightness change'
     exit 1
 fi
 
-SCREEN = xrandr | grep " connected" | cut -f1 -d "" | tail -1
-echo "$SCREEN"
-
-if [[ -z "$SCREEN" ]]
+if [[ "$1" -gt 100 ]] || [[ "$1" -lt 10 ]]
 then
-    echo "No SCREEN Found"
+    echo "Brightness must be between 10 - 100"
     exit 1
 fi
-echo "An expected brightness change of $1% for the $SCREEN"
 
-xrandr --output $SCREEN --brightness `expr $1 / 100`
+screen=$(xrandr | grep " connected" | cut -f1 -d " " | tail -1)
+
+if [[ -z "$screen" ]]
+then
+    echo "No screen Found"
+    exit 1
+fi
+echo "Brightness change for $1% on $screen"
+
+value=$(bc -l <<< 'scale=2;'$1'/100')
+
+xrandr --output $screen --brightness $value
