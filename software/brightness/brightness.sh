d2b9ce1e237aacb51070833c4f9e48f46d0b3efc
YvesCousteau <hugohenrotte@hotmail.com>
2023-03-08 14:10:42 +0100

    diming

--- software/brightness/brightness.sh
+++ software/brightness/brightness.sh
@@ -1,11 +1,6 @@
 #! /usr/bin/env bash
 # Brightness Change for Linux with xrandr (X11)
 
-if [[ $# -eq 0 ]] ; then
-    echo 'No brightness change'
-    exit 1
-fi
-
 if [[ "$1" -gt 100 ]] || [[ "$1" -lt 10 ]]
 then
     echo "Brightness must be between 10 - 100"
@@ -14,29 +9,18 @@ fi
 
 screen=$(xrandr | grep " connected" | cut -f1 -d " " | tail -1)
 
-if [[ -z "$screen" ]]
-then
-    echo "No screen Found"
-    exit 1
-fi
-echo "Brightness change for $1% on $screen"
-
-
 currentbrightness=$(xrandr --verbose | grep -i brightness | cut -f2 -d ' ' | head -n1)
 currentbrightness=$(bc -l <<< 'scale=2;'$currentbrightness'*100')
 currentbrightness=$( printf "%.0f" $currentbrightness )
-echo "$currentbrightness"
 
 while [ $1 != $currentbrightness ]
 do
-    echo "From $currentbrightness To $1"
     if [[ $1 -gt $currentbrightness ]]
     then
         currentbrightness=$(($currentbrightness+1))
     else
         currentbrightness=$(($currentbrightness-1))
     fi
-
     value=$(bc -l <<< 'scale=2;'$currentbrightness'/100')
     xrandr --output $screen --brightness $value
 done
